762041fafa11a5327c0ccbfa6ff049576dd9f48e
pranavbadhan8 <120732731+pranavbadhan8@users.noreply.github.com>
2023-05-02 22:27:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+}
 
-function Employee(name, age, jobTitle) {}
-
+function Employee(name, age, jobTitle) {
+	jobGreet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.years} years old, and my job title is ${this.jobTitle}`)
+	}
+}
+  
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
